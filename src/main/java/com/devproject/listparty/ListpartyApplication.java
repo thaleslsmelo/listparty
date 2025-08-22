6d59a5d5bd33988603ff0e65135907de1c4a023e
Thales <thaleslucasdemelo@gmail.com>
2025-08-22 13:29:34 -0300

    Project created

---
+++ src/main/java/com/devproject/listparty/ListpartyApplication.java
@@ -0,0 +1,13 @@
+package com.devproject.listparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListpartyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListpartyApplication.class, args);
+	}
+
+}
