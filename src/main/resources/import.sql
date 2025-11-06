384a0e33c8ec52b16535d4d75034fa8e376f145b
Thales <thaleslucasdemelo@gmail.com>
2025-11-06 12:28:41 -0300

    html, add imagens

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,7 +5,7 @@ INSERT INTO tb_product_List (names) VALUES ('Corporativos');
 
 INSERT INTO tb_product (names, price, category, ramo, img_url, short_description, long_description) VALUES ('Tabuleiro Guloseimas', 400.8, 'Tabul', 'Party inf','https://raw.githubusercontent.com/thaleslsmelo/listparty/refs/heads/main/src/main/resources/imagens/Fini.png', 'Curta Descriçao:', 'Longa Descrição');
 INSERT INTO tb_product (names, price, category, ramo, img_url, short_description, long_description) VALUES ('Tabuleiro Frutas', 470,'Tabul', 'Party inf','https://raw.githubusercontent.com/thaleslsmelo/listparty/refs/heads/main/src/main/resources/imagens/TabuFrutas.jpeg', 'Curta Descriçao:', 'Longa Descrição');
-INSERT INTO tb_product (names, price, category, ramo, img_url, short_description, long_description) VALUES ('Tabuleiro Brigadeiro', 450, 'Tabul', 'Party inf and corporativo','https://raw.githubusercontent.com/thaleslsmelo/listparty/refs/heads/main/src/main/resources/imagens/quizeanos.jpeg', 'Curta Descriçao:', 'Longa Descrição');
+INSERT INTO tb_product (names, price, category, ramo, img_url, short_description, long_description) VALUES ('Tabuleiro Brigadeiro', 450, 'Tabul', 'Party inf and corporativo','https://raw.githubusercontent.com/thaleslsmelo/listparty/refs/heads/main/src/main/resources/imagens/duplaPerson.png', 'Curta Descriçao:', 'Longa Descrição');
 INSERT INTO tb_product (names, price, category, ramo, img_url, short_description, long_description) VALUES ('Tabuleiro Corporativo', 350, 'Tabul', 'Party inf', 'https://raw.githubusercontent.com/thaleslsmelo/listparty/refs/heads/main/src/main/resources/imagens/corporativo.jpeg', 'Curta Descriçao:', 'Longa Descrição');
 INSERT INTO tb_product (names, price, category, ramo, img_url, short_description, long_description) VALUES ('Tabuleiro 15anos/Casamento', 460, 'Tabul', 'Festas 15Anos','https://raw.githubusercontent.com/thaleslsmelo/listparty/refs/heads/main/src/main/resources/imagens/quinzenaost.jpeg', 'Curta Descriçao:', 'Longa Descrição');
 INSERT INTO tb_product (names, price, category, ramo, img_url, short_description, long_description) VALUES ('Carrinho', 40.7, 'Exper', 'Party inf,casamentos ','https://raw.githubusercontent.com/thaleslsmelo/listparty/refs/heads/main/src/main/resources/imagens/carrinho.png', 'Curta Descriçao:', 'Longa Descrição');
@@ -17,7 +17,7 @@ INSERT INTO tb_product (names, price, category, ramo, img_url, short_description
 INSERT INTO tb_belonging (prod_List_id, prod_id, position) VALUES (1, 1, 0);
 INSERT INTO tb_belonging (prod_List_id, prod_id, position) VALUES (1, 3, 1);
 INSERT INTO tb_belonging (prod_List_id, prod_id, position) VALUES (1, 2, 2);
-INSERT INTO tb_belonging (prod_List_id, prod_id, position) VALUES (1, 4, 3);
+
 
 
 INSERT INTO tb_belonging (prod_List_id, prod_id, position) VALUES (2, 1, 0);
